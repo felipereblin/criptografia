78292baa71718d1a69614c15bd5156449be74b9b
larissacensi <larissa.pelenci@catolicasc.org.br>
2020-03-20 19:26:08 -0300

    Chave privada e publica

---
+++ src/criptografia/ChavePublica.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.security.PublicKey;
+
+public class ChavePublica {
+	public PublicKey carregaChavePublica(File fPub) throws IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fPub));
+		PublicKey ret = (PublicKey) ois.readObject();
+		ois.close();
+		return ret;
+	}
+}
